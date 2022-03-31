41fafcba73d6e34b428dceb00f564a19620df53e
Martijn Heil <m.heil375@gmail.com>
2022-03-31 11:40:06 +0000

    gradle: remove incorrect isChanging for TAB dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -80,7 +80,7 @@ idea {
 
 dependencies {
     implementation("org.spigotmc:spigot-api:1.18.1-R0.1-SNAPSHOT") { isChanging = true }
-    implementation("me.neznamy:tab-api:3.0.2") { isChanging = true }
+    implementation("me.neznamy:tab-api:3.0.2")
     implementation("com.gitlab.martijn-heil:NinCommands:-SNAPSHOT") { isChanging = true }
     //compileOnly(fileTree("lib") { include("*.jar") })
     shadow(kotlin("stdlib"))
